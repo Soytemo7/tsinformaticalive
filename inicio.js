705ee79c5861b469efa24881fef31c9eb395aa43
Soytemo7 <contador.manuel0@gmail.com>
2025-07-26 17:06:38 -0600

    Clases TC

---
+++ inicio.js
@@ -0,0 +1,10 @@
+//  npm install typescript -D
+// installa extension typescript en vsc
+//  tsc --init
+// tsc hello.js
+// node hello.js
+function greet(name) {
+    return "hello";
+}
+var user = "world";
+console.log(greet(user));
---
+++ inicio.ts
@@ -0,0 +1,12 @@
+//  npm install -g typescript
+// installa extension typescript en vsc
+//  tsc --init
+// tsc hello.js
+// node hello.js
+
+function greet(name:string):string{
+    return "hello"
+}
+
+let user = "world";
+console.log(greet(user))
